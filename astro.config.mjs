58a5746f74ee8a603d2f7a8250bb6931a4a6e257
Louis <louis.huyle@outlook.com>
2025-12-14 00:41:26 +0700

    config CORS for astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,4 +12,10 @@ export default defineConfig({
 	vite: {
     	plugins: [tailwindcss()],
   	},
+	server: {
+    	headers: {
+      		"Cross-Origin-Opener-Policy": "same-origin",
+      		"Cross-Origin-Embedder-Policy": "require-corp",
+    	},
+  	},
 });
